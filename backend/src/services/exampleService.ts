88f4cfea5d7c6956c6539f79fa0278ad11d837f7
Krishnamurthy Ravi <krishnamurthy.ravi@bounteous.com>
2024-09-11 17:21:06 +0530

    remove unwanted file

--- backend/src/services/exampleService.ts
+++ backend/src/services/exampleService.ts
@@ -1,24 +1,10 @@
 // Import any required models here
-import { gpt } from "@/lib/gpt";
 import Example from "@/models/exampleModel";
-import { unescapeMarkDownString } from "@/utils/utils";
 import { readFileSync } from "fs";
 
 // Define your service methods
 export const getExamples = async () => {
-  const filePath =
-    "/home/krishnamurthyravi/Documents/video2blog-main/backend/uploads/66acc85265ece4fe408056b0_transcript_serial.txt";
-  const prompt =
-    "Convert this transcript into a blog in mark down format and send only that as a response.";
-
-  const content = await readFileSync(filePath, "utf-8");
-  const response = await gpt(prompt, content);
-  if(response.message.content){
-    const md = await unescapeMarkDownString(response.message.content);
-    return md;
-  } else {
-    return ""
-  }
+  return await Example.find({});
 };
 
 export const createExample = async (name: string) => {
